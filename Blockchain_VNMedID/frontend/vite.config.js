165c7b0fb9a40ca784e499500c00dd50948ab815
Violet <nguynhoanggiaviethung@gmail.com>
2026-06-14 13:16:53 +0700

    fix: vite allow all hosts

--- Blockchain_VNMedID/frontend/vite.config.js
+++ Blockchain_VNMedID/frontend/vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
-    allowedHosts: 'victorious-commitment-production-f268.up.railway.app'  // ← thêm dòng này
-  }
+    allowedHosts: ['all'],  // ← dùng array
+    strictPort: true,
+    }
 })
