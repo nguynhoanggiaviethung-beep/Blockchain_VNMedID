af28f30633cebedd41d76d30abe996c701c59330
Violet <nguynhoanggiaviethung@gmail.com>
2026-06-14 13:12:14 +0700

    up

--- Blockchain_VNMedID/frontend/vite.config.js
+++ Blockchain_VNMedID/frontend/vite.config.js
@@ -6,6 +6,6 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
-    allowedHosts: 'all'  // ← thêm dòng này
+    allowedHosts: 'victorious-commitment-production-f268.up.railway.app'  // ← thêm dòng này
   }
 })
