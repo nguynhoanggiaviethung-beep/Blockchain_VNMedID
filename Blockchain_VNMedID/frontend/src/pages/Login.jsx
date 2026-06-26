80ca1d21fdd69e008a2481db5f86e058f519657f
Violet <nguynhoanggiaviethung@gmail.com>
2026-06-26 15:09:15 +0700

    update sửa tiếp

--- Blockchain_VNMedID/frontend/src/pages/Login.jsx
+++ Blockchain_VNMedID/frontend/src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import { ethers } from "ethers";
 import logo from "../assets/logoVNMedID.png";
 import RegisterPatientForm from "./RegisterPatientForm";
 import { mapBackendToFrontend } from '../utils/doctorMapper.js';
